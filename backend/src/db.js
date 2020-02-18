a684d4804fa28e79f5990701645f894b4f2a0a47
Antonio Rivera <antonio.rivera@Sistemass-MacBook-Pro.local>
2020-02-18 12:44:06 +0100

    graphQl prisma working and serverrunning

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// this file connect to Primsa DB
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+
+const createServer = require('./createServer');
+const db = require('./db');
+const server = createServer();
+
+//TODO use express middlware to handle cookies (JWT) we need to see if we want to recover password from student
+//TODO use express middlware to populate current user -- (user ME)
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is running on port http:/localhost:${deets.port}`);
+  }
+);
