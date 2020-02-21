5535b7b3afa458af2db0995c0094997727fcb7be
Antonio Rivera <antonio.rivera@Sistemass-MacBook-Pro.local>
2020-02-21 13:06:10 +0100

    wip user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env' });
 
 const createServer = require('./createServer');
 const db = require('./db');
 const server = createServer();
 
-//TODO use express middlware to handle cookies (JWT) we need to see if we want to recover password from student
+server.express.use(cookieParser());
+
 //TODO use express middlware to populate current user -- (user ME)
 
 server.start(
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,33 @@
-const mutations = {};
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const mutations = {
+  async signup(parent, args, ctx, info) {
+    // email all write in lowCase
+    args.email = args.eamil.toLowerCase();
+    // Hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create de user in the DB
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    //  we set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year
+    });
+    // return the user create
+    return user;
+  }
+};
 
 module.exports = mutations;
