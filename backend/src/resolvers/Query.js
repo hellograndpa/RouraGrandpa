cf07ca529e1259575434992e3f68b82862a65cc0
Victor Camarena <vcamarena@gruponat.com>
2020-03-02 21:58:37 +0100

    housing

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,11 +1,6 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  // this is old way to do it.
-  // async typeUsers(parent, args, ctx, info) {
-  //   const typeUser = await ctx.db.query.typeUser();
-  //   return typeUser;
-  // }
   typeUsers: forwardTo('db')
 };
 
