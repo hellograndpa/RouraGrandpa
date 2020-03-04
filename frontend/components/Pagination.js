3e2bc3ab3a5229968e7700386558eaa6bf87c6ae
Antonio Rivera <antonio.rivera@Sistemass-MacBook-Pro.local>
2020-03-04 10:23:10 +0100

    wip: pagination

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -3,6 +3,32 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { perPage } from '../config';
 
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styled from 'styled-components';
+
+const PaginationStyles = styled.div`
+  text-align: center;
+  display: inline-grid;
+  grid-template-columns: repeat(4, auto);
+  align-items: stretch;
+  justify-content: center;
+  align-content: center;
+  margin: 2rem 0;
+  & > * {
+    margin: 0;
+    padding: 15px 30px;
+    &:last-child {
+      border-right: 0;
+    }
+  }
+  a[aria-disabled='true'] {
+    color: grey;
+    pointer-events: none;
+  }
+`;
+
 const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
     usersConnection {
@@ -17,11 +43,40 @@ const Pagination = props => (
   <div>
     <Query query={PAGINATION_QUERY}>
       {({ data, loading, error }) => {
+        const { page } = props;
         if (loading) return <p>Loading...</p>;
         if (error) return <p>Error: {error.message}</p>;
         const { count } = data.usersConnection.aggregate;
-        const pages = count / perPage;
-        return <p>page 1 of {pages}</p>;
+        const pages = Math.ceil(count / perPage);
+        return (
+          <PaginationStyles>
+            <Head>
+              <title>
+                HelloGrandpa - Usuarios - page {page} of {pages}
+              </title>
+            </Head>
+            <Link
+              prefetch
+              href={{
+                pathname: 'users',
+                query: { page: page - 1 }
+              }}
+            >
+              <a aria-disabled={page <= 1}> {'< '} Previo</a>
+            </Link>
+            <p>
+              page {page} of {pages}
+            </p>
+            <Link
+              href={{
+                pathname: 'users',
+                query: { page: page + 1 }
+              }}
+            >
+              <a aria-disabled={page === pages}>Siguiente {' >'}</a>
+            </Link>
+          </PaginationStyles>
+        );
       }}
     </Query>
   </div>
--- frontend/components/Users.js
+++ frontend/components/Users.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_USERS_QUERY = gql`
-  query ALL_USERS_QUERY {
-    users {
+  query ALL_USERS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    users(first: $first, skip: $skip, orderBy: createAt_DESC) {
       id
       name
       lastname
@@ -23,9 +24,15 @@ class Users extends Component {
     return (
       <div>
         <h2>User List</h2>
-        <Pagination></Pagination>
+        <Pagination page={this.props.page}></Pagination>
 
-        <Query query={ALL_USERS_QUERY}>
+        <Query
+          query={ALL_USERS_QUERY}
+          variables={{
+            skip: 2,
+            first: 4
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p> Error: {error.message}</p>;
@@ -58,7 +65,7 @@ class Users extends Component {
             );
           }}
         </Query>
-        <Pagination></Pagination>
+        <Pagination page={this.props.page}></Pagination>
       </div>
     );
   }
