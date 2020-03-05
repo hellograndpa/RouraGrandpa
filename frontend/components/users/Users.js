20798def8a26a6420832d6d05ce5cf73afc9991f
ittenes <antonioriver@gmail.com>
2020-03-05 16:30:00 +0100

    configuration eslint and prettier

--- frontend/components/users/Users.js
+++ frontend/components/users/Users.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import Pagination from './Pagination.component';
-import { perPage } from '../../config';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Pagination from "./Pagination.component";
+import { perPage } from "../../config";
+import styled from "styled-components";
 
 const CustomTable = styled.table`
-	&&& {
-		table,
-		th,
-		td {
-			border: 1px solid black;
-			border-collapse: collapse;
-		}
-		th,
-		td,
-		tr {
-			padding: 5px;
-		}
-		th {
-			text-align: left;
-		}
-		table {
-			width: 100%;
-		}
-	}
+  table,
+  th,
+  td {
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+  th,
+  td,
+  tr {
+    padding: 5px;
+  }
+  th {
+    text-align: left;
+  }
+  table {
+    width: 100%;
+  }
 `;
 
 const ALL_USERS_QUERY = gql`
@@ -43,60 +41,66 @@ const ALL_USERS_QUERY = gql`
 `;
 
 class Users extends Component {
-	render() {
-		return (
-			<div>
-				<h2>User List</h2>
-				<Pagination page={this.props.page} />
+  render() {
+    return (
+      <div>
+        <h2>User List</h2>
+        <Pagination page={this.props.page} />
 
-				<Query
-					query={ALL_USERS_QUERY}
-					fetchPolicy="network-only"
-					variables={{
-						skip: this.props.page * perPage - perPage,
-						first: perPage,
-					}}>
-					{({ data, error, loading }) => {
-						if (loading) return <p>Loading...</p>;
-						if (error) return <p> Error: {error.message}</p>;
+        <Query
+          query={ALL_USERS_QUERY}
+          fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage,
+            first: perPage
+          }}
+        >
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p> Error: {error.message}</p>;
 
-						return (
-							<CustomTable>
-								<table>
-									<thead>
-										<tr>
-											<th> id</th>
-											<th> Name</th>
-											<th> Last Name</th>
-											<th> Email</th>
-											<th> Phone </th>
-											<th> Type of User</th>
-										</tr>
-									</thead>
-									<tbody>
-										{data.users.map((user, index) => {
-											const { id, name, lastname, email, phone, typeUser: { typeName } } = user;
-											return (
-												<tr key={id}>
-													<td> {id}</td>
-													<td> {name}</td>
-													<td> {lastname}</td>
-													<td> {email}</td>
-													<td> {phone}</td>
-													<td> {typeName}</td>
-												</tr>
-											);
-										})}
-									</tbody>
-								</table>
-							</CustomTable>
-						);
-					}}
-				</Query>
-				<Pagination page={this.props.page} />
-			</div>
-		);
-	}
+            return (
+              <CustomTable>
+                <thead>
+                  <tr>
+                    <th> id</th>
+                    <th> Name</th>
+                    <th> Last Name</th>
+                    <th> Email</th>
+                    <th> Phone </th>
+                    <th> Type of User</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.users.map((user, index) => {
+                    const {
+                      id,
+                      name,
+                      lastname,
+                      email,
+                      phone,
+                      typeUser: { typeName }
+                    } = user;
+                    return (
+                      <tr key={id}>
+                        <td> {id}</td>
+                        <td> {name}</td>
+                        <td> {lastname}</td>
+                        <td> {email}</td>
+                        <td> {phone}</td>
+                        <td> {typeName}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </CustomTable>
+            );
+          }}
+        </Query>
+        <Pagination page={this.props.page} />
+      </div>
+    );
+  }
 }
 
 export default Users;
