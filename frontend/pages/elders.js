bde9e646c58da3d837e94d823e11e2f1e30f4904
ittenes <antonioriver@gmail.com>
2020-03-27 20:00:41 +0100

    fix: pagination in grandpa list

--- frontend/pages/elders.js
+++ frontend/pages/elders.js
@@ -5,7 +5,7 @@ import Elders from '../components/elder/Elders';
 const EldersPage = props => (
   <>
     <h2>This is the Users List</h2>
-    <Elders />
+    <Elders page={parseFloat(props.query.page) || 1} />
   </>
 );
 
